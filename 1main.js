3d17ef3422770948db81f3a07f0bc11c3a5446e3
safal219 <109123294+safal219@users.noreply.github.com>
2022-07-21 11:10:35 +0545

    Add files via upload

---
+++ 1main.js
@@ -0,0 +1,5 @@
+Confirm("Do You Want to Exit")  
+var a = prompt("Enter value of \n a;")
+var b = prompt("Enter value of \n b;")
+var C = Number(a) + Number(b);
+alert(`Addition is :${C}`);
---
+++ save.js
@@ -0,0 +1,39 @@
+function squre()
+{
+    var num =document.getElementById("n").value;
+    var res =num*num;
+    document.getElementById("result").innerHTML=res;
+   
+}
+function add()
+{
+var num =document.getElementById("a").value;
+var uo=num+num;
+document.getElementById("result").innerHTML=uo;
+}
+// hello//
+var a=true;
+var b=false;
+
+ //console.log(a ,b);
+ //
+ var und =undefined;
+ //console.log(undefined);
+ //
+ //
+ var a=null
+ //console.log(null);
+ //
+ var arr = [ 1,2,3,4,5]
+ //console.log(arr);
+ var a = 45
+ var b = 67
+ console.log("The value of a + b is",a+b);
+ console.log("The value of a - b is",a-b);
+ console.log("The value of a * b is",a*b);
+ console.log("The value of a / b is",a/b);
+ //Assigement operators 
+ var C=b;
+ C+=a;
+ console.log(b,f);
+
